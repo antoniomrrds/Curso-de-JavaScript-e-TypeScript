9c3e53c8d8e07b322707ca0a56cf0699f5d2ebb9
antoniomrrds <arqueiroverd@gmail.com>
2022-07-24 19:19:40 -0300

    modulo de js types

---
+++ js/tipos de dados/index.js
@@ -0,0 +1,9 @@
+//String, number, undefined, null, boolean, symbol
+const nome =  Antony // String
+const nomel ="Antony"// String
+const nome2 =`Luiz` // string
+const numl = l0; // number
+const num2 = 10.52; // number
+let nomeALuno; // undefined -> nao aponta pra local nenhum  na memoria
+const sobrenomeAluno = null; // Nul0 -> não aponta pra local nenhum na memória
+const aprovado = false // Boolean (verdadeiro ou falso)
